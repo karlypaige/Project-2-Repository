49562496485c726d4f3dd54177c3b20d78318239
Karly Maruyama <karlypaige@yahoo.com>
2021-01-19 19:46:56 -0800

    adding userDetails table (still needs to be connected to users table)

---
+++ models/userDetails.js
@@ -0,0 +1,29 @@
+module.exports = function (sequelize, DataTypes) {
+    const UserDetails = sequelize.define("UserDetails", {
+      first_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          len: [1,30]
+        }
+      },
+      last_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          len: [1,30]
+        }
+      },
+      user_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          len: [1,30]
+        }
+      }
+    });
+    return UserDetails;
+};
