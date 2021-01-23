2bbaafcb62839d07df4ffdb1d5a74de1fdc06ece
Karly Maruyama <karlypaige@yahoo.com>
2021-01-23 13:20:04 -0800

    moved code around to iterate through a list of questions and display the session score.  Working on storing score in database

--- public/js/trivia.js
+++ public/js/trivia.js
@@ -2,72 +2,98 @@ $(document).ready(() => {
     // This file just does a GET request to figure out which user is logged in
     // and updates the HTML on the page
     var score = 0;
+    var questArray = [];
+    var quest = $("#questions");
+        
 
     $("#play").on("click", function () {
         const diff = $("#difficulty").val();
 
-        getQuestions(undefined, undefined, diff)
+        getNewToken()
+            .then(() => {
+                getQuestions(10, undefined, diff)
+                    .then(function (questions) {
+                        // var quest = $("#questions")
+                        console.log(questions);
+                        questArray = questions;
 
-            .then(function (questions) {
-                var quest = $("#questions")
-                console.log(questions)
-                
-                quest.empty();
+                        renderQuestion();
+                    })
+                }
+            );
+    });
 
-                if (questions[0]["type"] === "multiple") {
-                    //array to hold multiple choice answers for shuffling
-                    var answers = [];
-                    
-                    answers.push(questions[0]["correct_answer"]);
-                    answers.push(questions[0]["incorrect_answers"][0]);
-                    answers.push(questions[0]["incorrect_answers"][1]);
-                    answers.push(questions[0]["incorrect_answers"][2]);
-                    console.log(answers);
-                    //shuffle the answers of the question
-                    answers = shuffleArray(answers);
-                    console.log(answers);
+    function renderQuestion() {
+        question = questArray.pop();
 
-                    quest.html(`<p>${questions[0]["question"]}</p>
+        
+
+        console.log(question);
+
+        if (question["type"] === "multiple") {
+            //array to hold multiple choice answers for shuffling
+            var answers = [];
+            answers.push(question["correct_answer"]);
+            answers.push(question["incorrect_answers"][0]);
+            answers.push(question["incorrect_answers"][1]);
+            answers.push(question["incorrect_answers"][2]);
+
+            //shuffle the answers of the question
+            answers = shuffleArray(answers);
+
+            //display question and shuffled answers
+            quest.html(`<p>${question["question"]}</p>
             <p><button class="answer">${answers[0]}</button>
             <button class="answer">${answers[1]}</button>
             <button class="answer">${answers[2]}</button>
             <button class="answer">${answers[3]}</button><p>`)
-                } else if (questions[0]["type"] === "boolean") {
-                    quest.html(`<p>${questions[0]["question"]}</p><p><button class="answer">TRUE</button> <button class="answer">FALSE</button></p>`)
-                }
-                else {
-                };
+        } else if (question["type"] === "boolean") {
+            //display true/false question and answers
+            quest.html(`<p>${question["question"]}</p>
+            <p><button class="answer">TRUE</button> 
+            <button class="answer">FALSE</button></p>`)
+        }
+        else {
+            // if(questions[i])
+        };
 
-                $(".answer").on("click", function (event) {
-                    console.log(event.target.textContent);
-                    if (event.target.textContent.toUpperCase() === questions[0]["correct_answer"].toUpperCase()) {
-                        // quest.empty();
-                        quest.html(`<p>"YOU ARE RIGHT"</p>`);
-                        score += 1;
-                    } else {
-                        quest.html(`<p>"NO!!"</p>`);
-                    }
-                })
+        $(".answer").on("click", function (event) {
+            console.log(event.target.textContent);
+            if (event.target.textContent.toUpperCase() === question["correct_answer"].toUpperCase()) {
+                // quest.empty();
+                quest.html(`<p>"YOU ARE RIGHT"</p>`);
+                score += 1;
+            } else {
+                quest.html(`<p>"NO!!"</p>`);
+            }
 
+            //if more questions display the next one
+            if (questArray.length != 0) {
+                renderQuestion();
+            } else {
+                //display session score
+                quest.html(`<p>"Your score is ${score}</p>`);
 
+                //store scores in database
+                
+            }
+        })
 
-            })
+    }
 
-    });
-
+    //function for shuffling an array
     function shuffleArray(array) {
         let curId = array.length;
         // There remain elements to shuffle
         while (0 !== curId) {
-          // Pick a remaining element
-          let randId = Math.floor(Math.random() * curId);
-          curId -= 1;
-          // Swap it with the current element.
-          let tmp = array[curId];
-          array[curId] = array[randId];
-          array[randId] = tmp;
+            // Pick a remaining element
+            let randId = Math.floor(Math.random() * curId);
+            curId -= 1;
+            // Swap it with the current element.
+            let tmp = array[curId];
+            array[curId] = array[randId];
+            array[randId] = tmp;
         }
         return array;
-      }
-
+    }
 });
