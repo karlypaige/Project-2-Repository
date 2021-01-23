af9d8080e67af71c3e98775bd6386c0808e6bc60
ducktrshessami <jwee761@gmail.com>
2021-01-23 11:38:11 -0800

    use email because name not guaranteed to be filled out

--- public/js/highscores.js
+++ public/js/highscores.js
@@ -4,7 +4,7 @@ $.get("/api/highscores")
     .then(scores => {
         let content;
         if (scores.length) {
-            content = scores.map(score => `<li>${score.score}<span class="float-right">${score.name}</span></li>`).join("\n");
+            content = scores.map(score => `<li>${score.score}<span class="float-right">${score.email}</span></li>`).join("\n");
         }
         else {
             content = "No scores yet!";
