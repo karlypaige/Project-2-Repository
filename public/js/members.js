9692ec6b80ffb6e32c60a5bb451b4308f66bf198
ducktrshessami <jwee761@gmail.com>
2021-01-23 12:37:29 -0800

    fixed tabbing and defined constant jquery objects before doing anything else

--- public/js/members.js
+++ public/js/members.js
@@ -1,48 +1,48 @@
 $(document).ready(() => {
+  // Getting references to our form and input
+  const userDetailsForm = $("form.userDetails");
+  const fNameInput = $("input#fName-input");
+  const lNameInput = $("input#lName-input");
+  const uNameInput = $("input#uName-input");
+
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(data => {
     $(".member-name").text(data.email);
   });
+  
+  // When the signup button is clicked, we validate the email and password are not blank
+  userDetailsForm.on("submit", event => {
+    event.preventDefault();
+    const userData = {
+      firstName: fNameInput.val().trim(),
+      lastName: lNameInput.val().trim(),
+      userName: uNameInput.val().trim()
+    };
 
-    // Getting references to our form and input
-    const userDetailsForm = $("form.userDetails");
-    const fNameInput = $("input#fName-input");
-    const lNameInput = $("input#lName-input");
-    const uNameInput = $("input#uName-input");
-  
-    // When the signup button is clicked, we validate the email and password are not blank
-    userDetailsForm.on("submit", event => {
-      event.preventDefault();
-      const userData = {
-        firstName: fNameInput.val().trim(),
-        lastName: lNameInput.val().trim(),
-        userName: uNameInput.val().trim()
-      };
-  
-      if (!userData.firstName || !userData.lastName || !userData.userName) {
-        return;
-      }
-      // If we have all three names, run the storeUserDetails function
-      storeUserDetails(userData.firstName, userData.lastName, userData.userName);
-      fNameInput.val("");
-      lNameInput.val("");
-      uNameInput.val("");
-    });
-  
-    // Does a post to the signup route. If successful, we are redirected to the members page
-    // Otherwise we log any errors
-    function storeUserDetails(fName, lName, uName) {
-      $.post("/api/userDetails", {
-        firstName: fName,
-        lastName: lName,
-        userName: uName
-      })
-        .then(() => {
-          console.log("in the reroute to trivia")
-          window.location.replace("/trivia");
-          // If there's an error, handle it by throwing up a bootstrap alert
-        })
+    if (!userData.firstName || !userData.lastName || !userData.userName) {
+      return;
     }
+    // If we have all three names, run the storeUserDetails function
+    storeUserDetails(userData.firstName, userData.lastName, userData.userName);
+    fNameInput.val("");
+    lNameInput.val("");
+    uNameInput.val("");
+  });
+
+  // Does a post to the signup route. If successful, we are redirected to the members page
+  // Otherwise we log any errors
+  function storeUserDetails(fName, lName, uName) {
+    $.post("/api/userDetails", {
+      firstName: fName,
+      lastName: lName,
+      userName: uName
+    })
+      .then(() => {
+        console.log("in the reroute to trivia")
+        window.location.replace("/trivia");
+        // If there's an error, handle it by throwing up a bootstrap alert
+      })
+  }
 
 });
